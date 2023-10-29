f29ff3826ada5c601101c72841e8ea7f110c9ac4
abhinavsiwal <abhinavsiwal@gmail.com>
2023-10-30 04:50:14 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app=express();
+var bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Import all the Routes
+
+module.exports = app;
